d9fbacc2f64c954807d2a8fbef99ef8284dc6283
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-02-22 12:31:54 +0530

    Add files via upload

---
+++ power_of_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+      if(n==1) return true;
+      if(n<=0) return false;
+      while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+};
