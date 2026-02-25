e2aa8673a17d71ad69ddf4b86afc522baf840050
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-02-25 16:34:12 +0530

    Add files via upload

---
+++ Delete_a_node_LL.cpp
@@ -0,0 +1,23 @@
+
+ #include<iostream>
+ using namespace std;
+  struct ListNode {
+      int val;
+      ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp=node;
+        ListNode* prev;
+        while(temp->next!=NULL)
+        {
+            temp->val=temp->next->val;
+            prev=temp;
+            temp=temp->next;
+        }
+        prev->next=NULL;
+
+    }
+};
