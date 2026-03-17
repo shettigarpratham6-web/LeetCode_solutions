7d3446499f77b424b4eea723deaa3b8f27d85339
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-03-17 15:51:38 +0530

    Add files via upload

---
+++ 401.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+         vector<string>result;
+         for(int h=0;h<12;h++)
+         {
+            for(int m=0;m<60;m++)
+            {
+                if(__builtin_popcount(h)+__builtin_popcount(m)==turnedOn)
+                {
+                    string time=to_string(h)+":"+(m<10? "0":"")+to_string(m);
+                    result.push_back(time);
+                }
+            }
+         }
+     return result;
+    }
+};
