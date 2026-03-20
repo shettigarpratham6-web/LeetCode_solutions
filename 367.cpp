11f11bf8fa7a3dd26e7382b20e66b42530206690
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-03-20 09:59:42 +0530

    Add files via upload

---
+++ 367.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long left=1,right=num;
+        while(left<=right)
+        {
+            long long mid=(left+right)/2;
+            long long square=mid*mid;
+            if(square==num)
+            {
+                return true;
+            }
+            else if(square<num)
+            {
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+};
