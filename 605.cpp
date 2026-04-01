7b4797d2c71d03a905d7d667ae5b87c6bd06a85b
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-04-01 19:04:16 +0530

    Add files via upload

---
+++ 605.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int len = flowerbed.size();
+        int count = 0;
+
+        for(int i = 0; i < len; i++)
+        {
+            if(flowerbed[i] == 0)
+            {
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                int next = (i == len - 1) ? 0 : flowerbed[i + 1];
+
+                if(prev == 0 && next == 0)
+                {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+
+        if(count >= n)
+        {
+            return true;
+        }
+        return false;
+    }
+};
