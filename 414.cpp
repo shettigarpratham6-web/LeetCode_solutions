6be9fdeb35984e913dcb4398fc7922cc692a8b4e
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-04-16 21:09:37 +0530

    Add files via upload

---
+++ 414.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <climits>  
+using namespace std;
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+      long long first=LLONG_MIN,second=LLONG_MIN,third=LLONG_MIN;
+      for(int num:nums)
+      {
+        if(num==first || num==second || num==third) continue;
+        if(num>first)
+        {
+            third=second;
+            second=first;
+            first=num;
+        }
+        else if(num>second)
+        {
+            third=second;
+            second=num;
+        }
+        else if(num>third){
+            third=num;
+        }
+
+        
+      }
+      return (third==LLONG_MIN)? first:third;
+
+    }
+};
