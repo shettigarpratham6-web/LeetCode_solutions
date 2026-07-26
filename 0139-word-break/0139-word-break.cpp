7176df45413996c410affc254123ee38a264ac34
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-07-26 19:57:51 +0530

    Time: 0 ms (100%), Space: 11 MB (81.05%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-      int len=s.length();
-      int maxlen=0;
-      vector<bool>dp(len+1,false);
-      for(string &word:wordDict)
-      {
+     int len=s.length();
+     int maxlen=0;
+     for(string &word:wordDict)
+     {
         maxlen=max(maxlen,(int)word.length());
-      }
-      dp[0]=true;
-  unordered_set<string>wordSet(wordDict.begin(),wordDict.end());
-      for(int i=1;i<=len;i++)
-      {
+     }
+     vector<int>dp(len+1,false);
+     unordered_set<string>wordSet(wordDict.begin(),wordDict.end());
+     dp[0]=true;
+     for(int i=1;i<=len;i++)
+     {
         for(int j=i-1;j>=max(0,i-maxlen);j--)
         {
             if(dp[j] && wordSet.count(s.substr(j,i-j)))
@@ -20,8 +20,8 @@ public:
                 break;
             }
         }
-      }
-      return dp[len];
+     }
+     return dp[len];
 
     }
 };
