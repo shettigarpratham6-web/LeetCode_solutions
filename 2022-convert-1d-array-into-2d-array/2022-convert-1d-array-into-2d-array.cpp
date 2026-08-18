8d7d895d1bde906f48fc4d0a87215db87d467a20
Pratham <shettigarpratham6@gmail.com>
2026-08-18 23:11:50 +0530

    Time: 12 ms (48.13%), Space: 90 MB (60.13%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -7,8 +7,9 @@ public:
             return {};
         }
         vector<vector<int>>ans(m,vector<int>(n));
-        for (int i = 0; i < original.size(); ++i) {
-            ans[i / n][i % n] = original[i];
+        for(int i=0;i<original.size();++i)
+        {
+           ans[i/n][i%n]=original[i];
         }
         return ans;
     }
