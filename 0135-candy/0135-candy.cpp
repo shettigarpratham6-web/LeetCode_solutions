0e9c91916c316d8215e11d030bcdf66dc689b693
Pratham <shettigarpratham6@gmail.com>
2026-09-13 19:58:17 +0530

    Time: 0 ms (100%), Space: 71.4 MB (26.29%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        int sum=0;
         vector<int>arr(n,1);
+        int sum=0;
         for(int i=1;i<n;i++)
         {
             if(ratings[i]>ratings[i-1])
@@ -11,6 +11,7 @@ public:
                 arr[i]=arr[i-1]+1;
             }
         }
+
         for(int i=n-2;i>=0;i--)
         {
             if(ratings[i]>ratings[i+1])
@@ -24,5 +25,6 @@ public:
             sum=sum+arr[i];
         }
         return sum;
+
     }
 };
