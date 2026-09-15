86a04b29f4eec9156892381f8b99daac8b178651
Pratham <shettigarpratham6@gmail.com>
2026-09-15 22:21:35 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,21 +7,23 @@ public:
         int right=n-1;
         while(mid<=right)
         {
-            if(nums[mid]==0)
-            {
-                swap(nums[mid],nums[left]);
-                left++;
-                mid++;
-            }
-            else if(nums[mid]==1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums[mid],nums[right]);
-                right--;
-            }
+           if(nums[mid]==0)
+           {
+            swap(nums[left],nums[mid]);
+            left++;
+            mid++;
+           }
+           else if(nums[mid]==1)
+           {
+            mid++;
+           }
+           else
+           {
+            swap(nums[mid],nums[right]);
+            right--;
+           }
+
         }
+        
     }
 };
